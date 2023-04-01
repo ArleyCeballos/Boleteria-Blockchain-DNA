5aa42631735c16bd2d6cd669364e7dc6f702eb26
ArleyCeballos <wilmerarleyd@gmail.com>
2023-04-01 00:08:11 -0500

    Inicio contrato principal

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,11 +4,31 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract MyToken is ERC721, ERC721Burnable, Ownable {
-    constructor() ERC721("MyToken", "MTK") {}
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
-        _safeMint(to, tokenId);
+    struct TicketInfo{
+        uint256 id;
+        string ciudad;
+        string tipoEvento;
     }
+
+    struct ProjecPropierties{
+        address owner;
+    }
+    ProjecPropierties public pp;
+    TicketInfo public ticketInfo;
+ 
+
+    constructor() ERC721("Blockchain Ticket DNA ", "DNA") 
+    {
+        pp.owner = msg.sender;
+    }
+
+    function transferTicket(address to) public onlyOwner {
+        _mint(to, ticketInfo.id);
+    }
+    
+   
 }
